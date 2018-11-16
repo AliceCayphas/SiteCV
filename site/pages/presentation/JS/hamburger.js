1472624a109b00dff596c5190be01c509e069d8f
Cayphas <alice.cayphas@gmail.com>
2018-11-16 13:20:58 +0100

    Création des pages du site

---
+++ site/pages/presentation/JS/hamburger.js
@@ -0,0 +1,17 @@
+function toggleMenu(evt) {
+
+        nav.classList.toggle('visible');
+        btn.children[0].classList.toggle('fa-bars');
+        btn.children[0].classList.toggle('fa-times');
+      
+}
+
+var btn = document.querySelector('header button');
+var nav = document.querySelector('nav');
+var links = document.querySelectorAll('nav a');
+
+btn.addEventListener('click', toggleMenu); 
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', toggleMenu);
+}
