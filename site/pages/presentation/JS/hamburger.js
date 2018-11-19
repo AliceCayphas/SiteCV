ca370deba0239b56910e236f5e35e3a23b75a823
Cayphas <alice.cayphas@gmail.com>
2018-11-19 16:29:20 +0100

    Fin du menu hamburger, création des icones menu version mobile

--- site/pages/presentation/JS/hamburger.js
+++ site/pages/presentation/JS/hamburger.js
@@ -1,17 +1,21 @@
-var btn = document.querySelector('header button');
-var nav = document.querySelector('nav');
-var links = document.querySelectorAll('nav a');
-function toggleMenu(evt) {
+document.addEventListener("DOMContentLoaded", function(event) {
 
-        nav.classList.toggle('visible');
-        btn.children[0].classList.toggle('fa-bars');
-        btn.children[0].classList.toggle('fa-times');
-      
-}
+    var btn = document.querySelector('header button');
+    var nav = document.querySelector('nav');
+    var links = document.querySelectorAll('nav a');
+    function toggleMenu(evt) {
+
+            nav.classList.toggle('visible');
+            btn.children[0].classList.toggle('fa-bars');
+            btn.children[0].classList.toggle('fa-times');
+
+    }
 
 
-btn.addEventListener('click', toggleMenu); 
+    btn.addEventListener('click', toggleMenu); 
 
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', toggleMenu);
-}
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', toggleMenu);
+    }
+})
+    
